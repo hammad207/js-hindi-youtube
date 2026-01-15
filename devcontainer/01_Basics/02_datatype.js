ce2ca36c08da6e4f0ee651a5c523084957ed1a81
hammad207 <hammadjoyaa8@gmail.com>
2026-01-15 15:57:30 +0000

    datatype

---
+++ devcontainer/01_Basics/02_datatype.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as modern version
+//alert(3+3); we are using node js not browser
+
+console.log(3+3);
+console.log("hamad");
+
+let name = "hammad";
+let age = 18;
+let isloggedin = false;
+
+//number => 2 to power 53
+//bigint => larger than 2 to power 53
+//string => ""
+//bolean => true/false
+//null => standalone value
+//undefined => value not assigned
+//symbol => unique
+
+//object
+console.log(typeof hamad, typeof age);
+ console.log(typeof undefined);//undefined
+ console.log(typeof null);//object (bug)
+ 
