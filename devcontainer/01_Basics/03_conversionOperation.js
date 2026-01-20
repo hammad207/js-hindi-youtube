85dc47a4c966ef3e49d5345ad5891a4d201536cf
hammad207 <hammadjoyaa8@gmail.com>
2026-01-20 15:00:52 +0000

    operation done

--- devcontainer/01_Basics/03_conversionOperation.js
+++ devcontainer/01_Basics/03_conversionOperation.js
@@ -29,3 +29,25 @@ console.log(typeof stringNumber)
 //  console.log(typeof (score));
 //  console.log(score == null);
 //  console.log(score === null);
+
+//*****************************operations**************************************** */
+
+let value =  33
+let negValue = -value
+console.log(negValue);
+
+let str1 = "hello"
+let str2 = " hammad"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);// "12"
+console.log(1 + "2");//"12"
+console.log("1" + 2 + 2);// "122"
+console.log(1 + 2 + "2");//"32"
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+gameCounter--
+console.log(gameCounter);
